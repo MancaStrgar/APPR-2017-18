18d77ff0883cfcac8c5fffe96d406fff21c572aa
Manca Strgar <mancastrgar@gmail.com>
2017-12-27 18:05:57 +0100

    urejanje grafov

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,20 +1,16 @@
 # 3. faza: Vizualizacija podatkov
 
 #Graf 10 držav z največ uvrstitvami med top 4 na evropskih prvenstvih
-ggplot(ep.rezultati %>% group_by(DRZAVA) %>% summarise(stevilo = n()) %>%
+top10 <- ggplot(ep.rezultati %>% group_by(DRZAVA) %>% summarise(stevilo = n()) %>%
          arrange(desc(stevilo)) %>% top_n(10),
        aes(x = reorder(DRZAVA, -stevilo), y = stevilo)) + geom_col() +
   xlab("Država") + ylab("Število uvrstitev") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 #Graf povprešnjega števila metov na igro najboljših strelceu po letih
-ggplot(najboljsi.strelec, aes(x = LETO, y = TOCKE)) + geom_col() +
-  xlab("Leto") + ylab("Povprečje števila pik na tekmo najboljšega igralca") +
+tocke <- ggplot(najboljsi.strelec, aes(x = LETO, y = TOCKE)) + geom_line() +
+  xlab("Leto") + ylab("Povprečje števila pik na tekmo") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
-#povprečje najboljših strelcev od leta 1935-1945
-povprečja1 <- TOCKE %>% group_by(c(1935:1945)) %>%
-  summarise(povprecje1 = sum(TOCKE/4))
 
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
